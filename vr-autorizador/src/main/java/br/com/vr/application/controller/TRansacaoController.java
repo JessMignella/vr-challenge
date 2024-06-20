746d0ba8f280ab3480c60f4fd474f9fcbf8c515c
Jéssica Mignella <jeferigat@gmail.com>
2024-06-19 23:12:16 -0300

    adicionando SchemaProperty

--- vr-autorizador/src/main/java/br/com/vr/application/controller/TRansacaoController.java
+++ vr-autorizador/src/main/java/br/com/vr/application/controller/TRansacaoController.java
@@ -2,6 +2,10 @@ package br.com.vr.application.controller;
 
 import br.com.vr.domain.service.CartaoService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.SchemaProperties;
+import io.swagger.v3.oas.annotations.media.SchemaProperty;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
@@ -22,8 +26,16 @@ public class TRansacaoController {
             @ApiResponse(responseCode = "201", description = "Transação autorizada com sucesso"),
             @ApiResponse(responseCode = "422", description = "Erro ao processar a transação")
     })
+
     @PostMapping
-    public ResponseEntity<String> autorizarTransacao(@RequestBody Map<String, Object> payload) {
+    public ResponseEntity<String> autorizarTransacao( @SchemaProperties( {
+            @SchemaProperty(name = "numeroCartao", schema = @Schema(type = "string", example = "1234567890123456")),
+            @SchemaProperty(name = "senhaCartao", schema = @Schema(type = "string", example = "1234")),
+            @SchemaProperty(name = "valor", schema = @Schema(type = "number", format = "double", example = "100.0"))
+    })
+
+            @RequestBody Map<String, Object> payload) {
+
         String numeroCartao = (String) payload.get("numeroCartao");
         String senha = (String) payload.get("senhaCartao");
         double valor = (Double) payload.get("valor");
